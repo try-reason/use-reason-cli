6c18cc20ae2ab2488413e13f54c417cb62ee2b58
Inácio Mattos <inaciotm@hotmail.com>
2023-11-07 19:36:02 -0300

    initial commit

---
+++ sample/src/entrypoints/hello.ts
@@ -0,0 +1,24 @@
+import { reasonStream } from 'tryreason'
+
+interface City {
+  /** A two sentence description of the city */
+  description: string;  
+  points_of_interest: {
+    name: string;
+    description: string;
+    address: string;
+  }[];
+}
+
+// POST /hello
+export default async function* hello() {
+  const res = await fetch(`http://ip-api.com/json/`)
+  if (res.status !== 200) {
+    return new Response('Error', { status: 500 })
+  }
+  const { city } = await res.json()
+
+  for await (const output of reasonStream<City>(`Tell me about ${city}`)) {
+    yield output
+  }
+}
