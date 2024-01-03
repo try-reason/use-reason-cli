d97a8cd7b8c5998f9b7a03605a2f88560a3b0f59
Inácio Mattos <inaciotm@hotmail.com>
2024-01-03 07:28:53 -0300

    add: .reason.config.example.js

---
+++ sample/.reason.config.example.js
@@ -0,0 +1,11 @@
+/** @type {import('tryreason').ReasonConfig} */
+const config = {
+  projectName: '<your-project-name>',
+
+  openai: {
+    key: '<your-openai-key>',
+    defaultModel: '<your-default-model>'
+  },
+}
+
+export default config
