186e4f08189e0dfe0e63b21daa7bd0daa91f05d8
Inácio Mattos <inaciotm@hotmail.com>
2025-11-25 20:07:05 -0800

    chore: fix openai missing config

--- sample/.reason.config.js
+++ sample/.reason.config.js
@@ -2,9 +2,11 @@
 const config = {
   projectName: '<your-project-name>',
 
-  baseURL: 'https://api.openai.com/v1/responses',
-	key: '<your-openai-key>',
-	defaultModel: 'gpt-4o'
+  openai: {
+    baseURL: 'https://api.openai.com/v1/responses',
+    key: '<your-openai-key>',
+    defaultModel: 'gpt-4o'
+  }
 }
 
 export default config
